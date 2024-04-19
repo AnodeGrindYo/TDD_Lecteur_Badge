452d1a2a4a45f6abf9fbc0a171d0b88f6bf18498
Djebali <younesdjbl6@gmail.com>
2024-04-19 15:40:09 +0200

    chnage badge class

--- src/badge.py
+++ src/badge.py
@@ -1,4 +1,5 @@
-
 class Badge():
-    def __init__(self):
-        self.identifiant = None
+    def __init__(self, identifiant, nom,prenom):
+        self.identifiant = identifiant
+        self.nom = nom
+        self.prenom = prenom
