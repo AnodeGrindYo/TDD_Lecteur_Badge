b6e5f917b674e27c80d3d05ef134b8a55fd7deaa
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-04-22 09:58:17 +0200

    2. test deux portes et une seul lecteur detexte carte

--- test/test_controle_acces.py
+++ test/test_controle_acces.py
@@ -31,3 +31,24 @@ def test_lecteur_non_detecte_badge_et_ne_pas_ouvrir_porte():
     moteur_ouverture.interroger()
 # ALORS le signal d'ouverture n'est pas envoyé à la porte
     assert not porte_spy.porte_ouverte
+
+## Cas deux portes
+def test_deux_portes_un_seul_lecteur_detecte_badge():
+    # ETANT DONNE un Lecteur ayant détecté un Badge
+    # ET un autre Lecteur n'ayant rien détecté
+    # ET une Porte reliée chacune à un Lecteur
+    porte_spy1 = PorteSpy()
+    porte_spy2 = PorteSpy()
+
+    lecteur_fake = LecteurFake()
+
+    moteur_ouverture = MoteurOuverture()
+    moteur_ouverture.associer(lecteur_fake, porte_spy1)
+
+    lecteur_fake.simuler_detection_badge()
+    # QUAND le Moteur d'ouverture effectue une interrogation des lecteurs
+    moteur_ouverture.interroger()
+    # ALORS la première porte est ouverte
+    # ET la deuxième porte reste fermée
+    assert porte_spy1.porte_ouverte
+    assert not porte_spy2.porte_ouverte
